785162f99f0f76e6d42e1431a86295fdf5eb5e9e
introversh <introversh@gmail.com>
2024-04-30 15:59:31 +0530

    first commit

---
+++ thinkTankers/home.js
@@ -0,0 +1,18 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.style.display === "none" ? dropdown.style.display = "block" : dropdown.style.display = "none";
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
---
+++ thinkTankers/script.js
@@ -0,0 +1,21 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    // Perform login authentication logic here
+    // For demonstration purposes, let's just redirect to the home page
+    window.location.href = "home.html";
+}
